dc6514f1c5bdb642e2f96b5b51add22de0a18eec
Ronald Head <ronaldlhead@gmail.com>
2022-04-21 17:17:11 -0600

    Add BrowserRouter to Index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
-  
-);
+const router = <BrowserRouter> <App /> </BrowserRouter>;
+
+ReactDOM.render(router, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
