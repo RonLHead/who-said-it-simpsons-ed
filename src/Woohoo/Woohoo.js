86e6ac9ff801607c219ca3465be4663a54bad4d7
Ronald Head <ronaldlhead@gmail.com>
2022-04-22 11:17:34 -0600

    Set up container compoennt for Woohoo

--- src/Woohoo/Woohoo.js
+++ src/Woohoo/Woohoo.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Woohoo = () => {
+const Woohoo = (props) => {
     return (
         <section>
             <h3>Woohoo!</h3>
             <h4>+1</h4>
             <NavLink to='/home'>
-                <button>Play again!</button>
+                <button onClick={()=>{
+                    props.updateWoohooScore();
+                }}>Play again!</button>
             </NavLink>
         </section>
     )
