eb835730e4089ca4c24f985e2a2b31c2d39eb41d
Ronald Head <ronaldlhead@gmail.com>
2022-04-21 19:55:42 -0600

    Set up Route to home and favoritequotes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,45 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Quote from '../Quote/Quote';
+import FavoriteQuotes from '../FavoriteQuotes/FavoriteQuotes';
+import { NavLink, Route, Redirect, useLocation } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      woohoo: 0,
-      doh: 0,
-      favoriteQuotes: []
-    }
-  }
+const App = () => {
+  const [woohoo, setWoohoo] = useState(0);
+  const [doh, setDoh] = useState(0);
+  const [favQuotes, setFavQuotes] = useState('');
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Who Said It?</h1>
-          <img className='App-logo' alt='simpsons-logo' src='pnghost_bart-simpson-marge-simpson-logo-simpsons.png'/>
-        </header>
-        <nav>
-          <h2>Woohoo: {this.state.woohoo}</h2>
-          <h2>D'oh: {this.state.doh}</h2>
-          <h2>Fav Quotes</h2>
-        </nav>
+  const location = useLocation()
+  console.log(location.pathname)
+  
+  // let button = {home} ? <NavLink to='/home'><h2 className='fav-quotes'>Fav Quotes</h2></NavLink> : <NavLink to='/favorite-quotes'><h2 className='fav-quotes'>Home</h2></NavLink>;
+  return (
+    <main className="App">
+      <header>
+        <h1>Who Said It?</h1>
+        <img className='App-logo' alt='simpsons-logo' src='pnghost_bart-simpson-marge-simpson-logo-simpsons.png'/>
+      </header>
+      <nav className='navbar'>
+        <h2>Woohoo: {woohoo}</h2>
+        <h2>D'oh: {doh}</h2>
+        {location.pathname === '/favorite-quotes'
+           ? (<NavLink to='/home' style={{textDecoration: 'none'}}>
+              <h2>Home</h2>
+            </NavLink>)
+          : (<NavLink to='/favorite-quotes' style={{textDecoration: 'none'}}>
+              <h2>Fav&nbsp;Quotes</h2>
+          </NavLink>)
+        }
+      </nav>
+      <Redirect exact from='/' to='/home' />
+      <Route exact path='/home'>
         <Quote />
-      </main>
-    );
-  }
+      </Route>
+      <Route exact path='/favorite-quotes'>
+        <FavoriteQuotes />
+      </Route>
+    </main>
+  );
 }
 
 export default App;
