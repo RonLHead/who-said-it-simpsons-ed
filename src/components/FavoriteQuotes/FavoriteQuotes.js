855e5e5cc50339fe2eb76d0eb7efe9570f34f225
Ronald Head <ronaldlhead@gmail.com>
2022-04-22 12:41:51 -0600

    Set up mapping of favorite quotes prop to display in Favorite Quotes component

--- src/components/FavoriteQuotes/FavoriteQuotes.js
+++ src/components/FavoriteQuotes/FavoriteQuotes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './FavoriteQuotes.css';
 
-const FavoriteQuotes = () => {
+const FavoriteQuotes = (props) => {
+    console.log(props)
+    const favoriteQuotesSet = props.favQuotes.map((quote, i)=> {
+        return (
+            <div key={i}>
+                <p>{quote}</p>
+            </div>
+        )
+    })
     return (
         <section>
-            <p>Quote 1: .............</p>
-            <p>Quote 2: .............</p>
-            <p>Quote 3: .............</p>
-            <p>Quote 4: .............</p>
+            {favoriteQuotesSet}
         </section>
         
     )
