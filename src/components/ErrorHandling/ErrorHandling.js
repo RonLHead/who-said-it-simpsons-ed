0e5603a0735427c4cef8a2a53095e4b96add1428
Ronald Head <ronaldlhead@gmail.com>
2022-04-24 00:48:21 -0600

    Add ErrorHandling component

---
+++ src/components/ErrorHandling/ErrorHandling.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import '../App/App.css';
+
+
+const ErrorHandling = ({ error }) => {
+    return (
+        <div>
+            <h2 >{error}</h2>
+        </div>
+    )
+}
+
+export default ErrorHandling;
